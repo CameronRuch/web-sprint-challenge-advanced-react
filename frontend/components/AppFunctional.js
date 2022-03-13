486ac437bf606e300321e6ba6c62ffd32da0aae1
CameronRuch <cameron.ruch@yahoo.com>
2022-03-13 01:15:17 -0600

    completed functional component

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -38,10 +38,10 @@ export default function AppFunctional(props) {
   }
 
   const upClick = () => {
-    if (coordinate.y < 3) {
+    if (coordinate.y > 1) {
       setSteps(steps + 1)
       setCoordinate({
-        ...coordinate, "y": coordinate.y + 1
+        ...coordinate, "y": coordinate.y - 1
       })
       setMessage('')
     } else {
@@ -50,10 +50,10 @@ export default function AppFunctional(props) {
   }
 
   const downClick = () => {
-    if (coordinate.y > 1) {
+    if (coordinate.y < 3) {
       setSteps(steps + 1)
       setCoordinate({
-        ...coordinate, "y": coordinate.y - 1
+        ...coordinate, "y": coordinate.y + 1
       })
       setMessage('')
     } else {
@@ -74,6 +74,7 @@ export default function AppFunctional(props) {
     axios.post('http://localhost:9000/api/result', newPost)
     .then( res => {
       setMessage(res.data.message)
+      setEmail('')
     })
     .catch(err => setMessage(err.response.data.message))
   }
@@ -90,18 +91,18 @@ export default function AppFunctional(props) {
     <div id="wrapper" className={props.className}>
       <div className="info">
         <h3 id="coordinates">{`Coordinates (${coordinate.x}, ${coordinate.y})`}</h3>
-        <h3 id="steps">You moved {steps} times</h3>
+        <h3 id="steps">You moved {steps} { steps === 1 ? "time" : "times" }</h3>
       </div>
       <div id="grid">
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square active">B</div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
+        <div className={coordinate.x === 1 && coordinate.y === 1 ? 'square active' : 'square'}>{ coordinate.x === 1 && coordinate.y === 1 ? 'B' : ''}</div>
+        <div className={coordinate.x === 2 && coordinate.y === 1 ? 'square active' : 'square'}>{ coordinate.x === 2 && coordinate.y === 1 ? 'B' : ''}</div>
+        <div className={coordinate.x === 3 && coordinate.y === 1 ? 'square active' : 'square'}>{ coordinate.x === 3 && coordinate.y === 1 ? 'B' : ''}</div>
+        <div className={coordinate.x === 1 && coordinate.y === 2 ? 'square active' : 'square'}>{ coordinate.x === 1 && coordinate.y === 2 ? 'B' : ''}</div>
+        <div className={coordinate.x === 2 && coordinate.y === 2 ? 'square active' : 'square'}>{ coordinate.x === 2 && coordinate.y === 2 ? 'B' : ''}</div>
+        <div className={coordinate.x === 3 && coordinate.y === 2 ? 'square active' : 'square'}>{ coordinate.x === 3 && coordinate.y === 2 ? 'B' : ''}</div>
+        <div className={coordinate.x === 1 && coordinate.y === 3 ? 'square active' : 'square'}>{ coordinate.x === 1 && coordinate.y === 3 ? 'B' : ''}</div>
+        <div className={coordinate.x === 2 && coordinate.y === 3 ? 'square active' : 'square'}>{ coordinate.x === 2 && coordinate.y === 3 ? 'B' : ''}</div>
+        <div className={coordinate.x === 3 && coordinate.y === 3 ? 'square active' : 'square'}>{ coordinate.x === 3 && coordinate.y === 3 ? 'B' : ''}</div>
       </div>
       <div className="info">
         <h3 id="message">{message}</h3>
